056f991224a454abc1d6291d203f7e9584968dd0
homchou <chouhom@sina.com>
2017-11-17 12:19:17 +0800

    d

--- webcrawler/data/start.sh
+++
@@ -1,9 +0,0 @@
-echo "172.31.12.214    crawl-dis1" >> /etc/hosts
-echo "172.31.2.36     crawl-dis2" >> /etc/hosts
-echo "172.31.13.69    crawl-dis3" >> /etc/hosts
-# echo "54.223.214.42    crawl-dis1" >> /etc/hosts
-# echo "52.80.9.247     crawl-dis2" >> /etc/hosts
-# echo "54.223.173.112    crawl-dis3" >> /etc/hosts
-#cd /data 
-rm -f twistd.pid
-scrapyd
